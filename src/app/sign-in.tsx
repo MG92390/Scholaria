c707ad22e1eb005a817d68a8b52a8c7eb6be0a71
Dracocybertech <pointdujourkh@gmail.com>
2026-01-23 16:04:14 +0100

    Add protected routes for auth

---
+++ src/app/sign-in.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
+
+export default function SignInScreen() {
+
+    return (
+        <View>
+            <Text>
+                Sign In Screen
+            </Text>
+        </View>
+    );
+}
